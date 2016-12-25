ee5cb3579bfedf93273b9318c1d50b946451756d
Habib Y.H <nucleusae@gmail.com>
2016-12-25 06:16:38 +0400

    updates

--- ircwx_parser.ts
+++ ircwx_parser.ts
@@ -50,6 +50,27 @@ class IRCmUser {
     ignore: boolean = false;
 }
 
+//ToDo: move to core library.
+type fnWriteToPresenterDef = (str: string) => void;
+//ToDo: move to core library.
+type fnWriteToConnectionDef = (str: string) => void;
+
+// <global variables>
+let DebugArray: string[];
+let fnWriteToPresenter: fnWriteToPresenterDef;
+let IRCSend: fnWriteToConnectionDef; //ToDo: rename later to fnWriteToConnection
+// </global variables>
+
+function AddtoDebugArray(s: string) { //-- Function converstion completed 25-Dec-2016 HY
+    DebugArray.push(s);
+    if (DebugArray.length > 50) DebugArray.splice(0, 1);
+}
+
+function handleError(sError) { //-- Function converstion completed 25-Dec-2016 HY
+    //ToDo: move out presentation logic from parser.
+    Write("<font color='#FF0000'>Error: " + sError + "</font>");
+}
+
 function parseJoin(userstr, flags, chan) { //-- Function converstion completed 19-Dec-2016 HY
 
     let oUser = new IRCmUser();
@@ -111,10 +132,10 @@ function parseJoin(userstr, flags, chan) { //-- Function converstion completed 1
     //onJoin(oUser, chan.substr(1)); //strip colon before channel name
 }
 
-function parseString(raw) {
+function parseString(raw) { //-- Function converstion partial complete 25-Dec-2016 HY
 
     if (raw.length > 0) {
-        var toks = [];
+        var toks: string[] = [];
         var ircmsg = (raw.charAt(0) == ":") ? raw.substr(1) : raw;
 
         //trace incoming
@@ -131,222 +152,231 @@ function parseString(raw) {
                 return;
 
             case "ping":
-                PingReply(toks.slice(1));
+                PingReply(toks[1]);
                 return;
         }
         // End of switch
 
-        switch (toks[1].toLowerCase()) {
-            case "001": //Welcome to the Internet Relay Network
-                ServerName = toks[0];
-                this.UserName = toks[2];
-                onSetNick(this.UserName);
-                _bConnectionRegistered = true;
-                GotoRoom();
-                break;
+        //conversion completed till here
 
-            case "251":
-                onNoticeServerMessage(toks.slice(3).join(" ").substr(1));
-                break;
+        //switch (toks[1].toLowerCase()) {
+        //    case "001": //Welcome to the Internet Relay Network
+        //        ServerName = toks[0];
+        //        this.UserName = toks[2];
+        //        onSetNick(this.UserName);
+        //        _bConnectionRegistered = true;
+        //        GotoRoom();
+        //        break;
 
-            case "265":
-                onNoticeServerMessage(toks.slice(3).join(" ").substr(1));
-                break;
+        //    case "251":
+        //        onNoticeServerMessage(toks.slice(3).join(" ").substr(1));
+        //        break;
 
-            case "join":
-                parseJoin(toks[0], toks[2], toks[3]);
-                break;
+        //    case "265":
+        //        onNoticeServerMessage(toks.slice(3).join(" ").substr(1));
+        //        break;
 
-            case "quit":
-                onQuit(getNick(toks[0]));
-                break;
+        //    case "join":
+        //        parseJoin(toks[0], toks[2], toks[3]);
+        //        break;
 
-            case "part":
-                onPart(getNick(toks[0]), toks[2]);
-                break;
+        //    case "quit":
+        //        onQuit(getNick(toks[0]));
+        //        break;
 
-            case "notice":
-                if (toks[0] == ServerName) {
-                    //Server Message
-                    if (!_bIsKicked) {
-                        if (toks[2] == "WARNING" && raw.indexOf("join a chatroom") > 0) GotoRoom();
-                    }
+        //    case "part":
+        //        onPart(getNick(toks[0]), toks[2]);
+        //        break;
 
-                    onNoticeServerMessage(toks.slice(2).join(" "));
-                } else if (toks[3].indexOf("%") == 0) {
-                    //channel broadcast
-                    onNoticeChanBroadcast(getNick(toks[0]), toks[3], strip(toks.slice(4).join(" ")));
-                } else if (toks[2].indexOf("%") < 0) {
-                    //server broadcast
-                    if (_bConnectionRegistered == true) onNoticeServerBroadcast(getNick(toks[0]), strip(toks.slice(3).join(" ")));
-                    else onNoticeServerMessage(toks.slice(2).join(" "));
-                } else if (toks[4].indexOf(":") == 0) {
-                    //private notice
-                    onNoticePrivate(getNick(toks[0]), toks[2], strip(toks.slice(4).join(" ")));
-                } else {
-                    //normal notice
-                    onNotice(getNick(toks[0]), toks[2], strip(toks.slice(3).join(" ")));
-                }
-                break;
+        //    case "notice":
+        //        if (toks[0] == ServerName) {
+        //            //Server Message
+        //            if (!_bIsKicked) {
+        //                if (toks[2] == "WARNING" && raw.indexOf("join a chatroom") > 0) GotoRoom();
+        //            }
 
-            case "kick":
-                if (toks[3].toLowerCase() == this.UserName.toLowerCase()) _bIsKicked = true; //use same case because server is case-insensitve for nicks.
-                //Write("MeKicked: KickedNick: " + toks[3] + "; clientNick: " + this.UserName + "; KickedFlag: " + _bIsKicked);
-                onKick(getNick(toks[0]), toks[2], toks[3], strip(toks.slice(4).join(" ")));
-                break;
+        //            onNoticeServerMessage(toks.slice(2).join(" "));
+        //        } else if (toks[3].indexOf("%") == 0) {
+        //            //channel broadcast
+        //            onNoticeChanBroadcast(getNick(toks[0]), toks[3], strip(toks.slice(4).join(" ")));
+        //        } else if (toks[2].indexOf("%") < 0) {
+        //            //server broadcast
+        //            if (_bConnectionRegistered == true) onNoticeServerBroadcast(getNick(toks[0]), strip(toks.slice(3).join(" ")));
+        //            else onNoticeServerMessage(toks.slice(2).join(" "));
+        //        } else if (toks[4].indexOf(":") == 0) {
+        //            //private notice
+        //            onNoticePrivate(getNick(toks[0]), toks[2], strip(toks.slice(4).join(" ")));
+        //        } else {
+        //            //normal notice
+        //            onNotice(getNick(toks[0]), toks[2], strip(toks.slice(3).join(" ")));
+        //        }
+        //        break;
 
-            case "privmsg":
-                if (toks[0].charAt(0) == "%") onChanPrivmsg(toks[0], toks[2], strip(toks.slice(3).join(" ")));
-                else if (toks[3].charAt(0) == ":") onPrivmsg(getNick(toks[0]), toks[2], toks.slice(3).join(" ").substr(1));
-                else onPrivmsgPr(getNick(toks[0]), toks[2], toks[3], toks.slice(4).join(" ").substr(1));
-                break;
+        //    case "kick":
+        //        if (toks[3].toLowerCase() == this.UserName.toLowerCase()) _bIsKicked = true; //use same case because server is case-insensitve for nicks.
+        //        //Write("MeKicked: KickedNick: " + toks[3] + "; clientNick: " + this.UserName + "; KickedFlag: " + _bIsKicked);
+        //        onKick(getNick(toks[0]), toks[2], toks[3], strip(toks.slice(4).join(" ")));
+        //        break;
 
-            case "whisper":
-                //Format> (:)>Test!0092132f753fba195ff8ce4f53704f74c8@masked WHISPER %#Test >Test2 :message
-                onWhisper(getNick(toks[0]), toks[2], toks[3], toks.slice(4).join(" ").slice(1));
-                break;
+        //    case "privmsg":
+        //        if (toks[0].charAt(0) == "%") onChanPrivmsg(toks[0], toks[2], strip(toks.slice(3).join(" ")));
+        //        else if (toks[3].charAt(0) == ":") onPrivmsg(getNick(toks[0]), toks[2], toks.slice(3).join(" ").substr(1));
+        //        else onPrivmsgPr(getNick(toks[0]), toks[2], toks[3], toks.slice(4).join(" ").substr(1));
+        //        break;
 
-            case "821": //unaway message
-                /*
-                    Formats>
-                        Personal> 	(:)<user> 821 :User unaway
-                        Channel> 	(:)<user> 821 <chan> :User unaway
-                */
+        //    case "whisper":
+        //        //Format> (:)>Test!0092132f753fba195ff8ce4f53704f74c8@masked WHISPER %#Test >Test2 :message
+        //        onWhisper(getNick(toks[0]), toks[2], toks[3], toks.slice(4).join(" ").slice(1));
+        //        break;
 
-                if (toks[2].indexOf("%") == 0) on821Chan(getNick(toks[0]), toks[2], toks.slice(3).join(" ").slice(1));
-                else on821Pr(getNick(toks[0]), toks.slice(2).join(" ").slice(1));
-                break;
+        //    case "821": //unaway message
+        //        /*
+        //            Formats>
+        //                Personal> 	(:)<user> 821 :User unaway
+        //                Channel> 	(:)<user> 821 <chan> :User unaway
+        //        */
 
-            case "822": //away message
-                /*
-                    Formats>
-                        Personal> 	(:)<user> 822 :<user message>
-                        Channel> 	(:)<user> 822 <chan> :<user message>
-                */
-                if (toks[2].indexOf("%") == 0) on822Chan(getNick(toks[0]), toks[2], toks.slice(3).join(" ").slice(1));
-                else on822Pr(getNick(toks[0]), toks.slice(2).join(" ").slice(1));
-                break;
+        //        if (toks[2].indexOf("%") == 0) on821Chan(getNick(toks[0]), toks[2], toks.slice(3).join(" ").slice(1));
+        //        else on821Pr(getNick(toks[0]), toks.slice(2).join(" ").slice(1));
+        //        break;
 
-            case "301":
-                on301(toks[3], toks.slice(4).join(" ").slice(1));
-                break;
+        //    case "822": //away message
+        //        /*
+        //            Formats>
+        //                Personal> 	(:)<user> 822 :<user message>
+        //                Channel> 	(:)<user> 822 <chan> :<user message>
+        //        */
+        //        if (toks[2].indexOf("%") == 0) on822Chan(getNick(toks[0]), toks[2], toks.slice(3).join(" ").slice(1));
+        //        else on822Pr(getNick(toks[0]), toks.slice(2).join(" ").slice(1));
+        //        break;
 
-            case "302":
-                on302(toks[2], toks[3].slice(1));
-                break;
+        //    case "301":
+        //        on301(toks[3], toks.slice(4).join(" ").slice(1));
+        //        break;
 
-            case "353": //names list reply
-                parseNamesList(ircmsg);
-                break;
+        //    case "302":
+        //        on302(toks[2], toks[3].slice(1));
+        //        break;
 
-            case "366": //names list end reply
-                onEndofNamesList();
-                break;
+        //    case "353": //names list reply
+        //        parseNamesList(ircmsg);
+        //        break;
 
-            case "324": //channel modes reply
-                parse324(toks[3], toks[4], toks[5], toks);
-                break;
+        //    case "366": //names list end reply
+        //        onEndofNamesList();
+        //        break;
 
-            case "433": //nick already in use error
-                //Format> (:)ChatDriveIrcServer.1 433 >Test >Test :Nickname is already in use
-                onErrorReplies(toks[1], toks[2], toks[3], strip(toks.slice(4).join(" ")));
-                if (UserName.length < 40) UserName = UserName + random(20000);
-                else {
-                    UserName = UserName.substr(0, 32);
-                    UserName = UserName + random(20000);
-                }
-                if (_bConnectionRegistered == false) onSetNick(UserName);
+        //    case "324": //channel modes reply
+        //        parse324(toks[3], toks[4], toks[5], toks);
+        //        break;
 
-                IRCSend("NICK " + UserName);
+        //    case "433": //nick already in use error
+        //        //Format> (:)ChatDriveIrcServer.1 433 >Test >Test :Nickname is already in use
+        //        onErrorReplies(toks[1], toks[2], toks[3], strip(toks.slice(4).join(" ")));
+        //        if (UserName.length < 40) UserName = UserName + random(20000);
+        //        else {
+        //            UserName = UserName.substr(0, 32);
+        //            UserName = UserName + random(20000);
+        //        }
+        //        if (_bConnectionRegistered == false) onSetNick(UserName);
 
-                if (_IsAuthRequestSent) sendAuthInfo();
-                break;
+        //        IRCSend("NICK " + UserName);
 
-            case "nick":
-                //Format> (:)>Test!0092132f753fba195ff8ce4f53704f74c8@masked NICK :>Test10555
-                onNick(getNick(toks[0]), strip(toks[2]));
-                break;
+        //        if (_IsAuthRequestSent) sendAuthInfo();
+        //        break;
 
-            case "authuser":
-                _IsAuthRequestSent = true;
+        //    case "nick":
+        //        //Format> (:)>Test!0092132f753fba195ff8ce4f53704f74c8@masked NICK :>Test10555
+        //        onNick(getNick(toks[0]), strip(toks[2]));
+        //        break;
 
-                sendAuthInfo();
-                break;
+        //    case "authuser":
+        //        _IsAuthRequestSent = true;
 
-            case "mode":
-                //parseMode(sFrom, sChan, sModes, sParam, aModes)
-                //printToks(toks);
-                parseMode(getNick(toks[0]), toks[2], toks[3], toks[4], toks);
-                break;
+        //        sendAuthInfo();
+        //        break;
 
-            case "341": //invite confirmation
-                on341(toks[2], toks[3], toks[4]);
-                break;
+        //    case "mode":
+        //        //parseMode(sFrom, sChan, sModes, sParam, aModes)
+        //        //printToks(toks);
+        //        parseMode(getNick(toks[0]), toks[2], toks[3], toks[4], toks);
+        //        break;
 
-            case "invite":
-                onInvite(getNick(toks[0]), toks[2], strip(toks[3]));
-                break;
+        //    case "341": //invite confirmation
+        //        on341(toks[2], toks[3], toks[4]);
+        //        break;
 
-            case "data":
-                /*
-                    :<servername> DATA <nickby> <type> :<message>
-                    :<servername> DATA <nickby> PID :<nickof> <pid>
-                    :user@masked DATA <channel> <userto> <tag> :<message>
-                */
-                onDataIRC(toks[2], toks[3], strip(toks.slice(4).join(" ")));
-                onDataIRC2(getNick(toks[0]), toks[2], toks[3], toks[4], strip(toks.slice(5).join(" ")));
-                break;
+        //    case "invite":
+        //        onInvite(getNick(toks[0]), toks[2], strip(toks[3]));
+        //        break;
 
-            case "knock":
-                onKnock(toks[0], toks[2], strip(toks.slice(3).join(" ")));
-                break;
+        //    case "data":
+        //        /*
+        //            :<servername> DATA <nickby> <type> :<message>
+        //            :<servername> DATA <nickby> PID :<nickof> <pid>
+        //            :user@masked DATA <channel> <userto> <tag> :<message>
+        //        */
+        //        onDataIRC(toks[2], toks[3], strip(toks.slice(4).join(" ")));
+        //        onDataIRC2(getNick(toks[0]), toks[2], toks[3], toks[4], strip(toks.slice(5).join(" ")));
+        //        break;
 
-            case "prop":
-                onProp(getNick(toks[0]), toks[2], toks[3], strip(toks.slice(4).join(" ")));
-                break;
+        //    case "knock":
+        //        onKnock(toks[0], toks[2], strip(toks.slice(3).join(" ")));
+        //        break;
 
-            case "332":
-                on332(toks[3], strip(toks.slice(4).join(" ")));
-                break;
+        //    case "prop":
+        //        onProp(getNick(toks[0]), toks[2], toks[3], strip(toks.slice(4).join(" ")));
+        //        break;
 
-            case "801": //IRCRPL_ACCESSADD
-            case "802": //IRCRPL_ACCESSDELETE
-            case "803": //IRCRPL_ACCESSSTART
-            case "804": //IRCRPL_ACCESSLIST
-            case "805": //IRCRPL_ACCESSEND
-            case "820": //IRCRPL_ACCESSEND
-            case "903": //IRCERR_BADLEVEL
-            case "913": //IRCERR_NOACCESS
-            case "914": //IRCERR_DUPACCESS
-            case "915": //IRCERR_MISACCESS
-            case "916": //IRCERR_TOOMANYACCESSES
-                onAccessNRelatedReplies(toks[1], ircmsg);
-                break;
+        //    case "332":
+        //        on332(toks[3], strip(toks.slice(4).join(" ")));
+        //        break;
 
-            case "900": //IRCERR_BADCOMMAND
-            case "901": //IRCERR_TOOMANYARGUMENTS
-            case "925": //IRCERR_TOOMANYARGUMENTS
-                onAccessNRelatedReplies(toks[1], ircmsg);
-                break;
+        //    case "801": //IRCRPL_ACCESSADD
+        //    case "802": //IRCRPL_ACCESSDELETE
+        //    case "803": //IRCRPL_ACCESSSTART
+        //    case "804": //IRCRPL_ACCESSLIST
+        //    case "805": //IRCRPL_ACCESSEND
+        //    case "820": //IRCRPL_ACCESSEND
+        //    case "903": //IRCERR_BADLEVEL
+        //    case "913": //IRCERR_NOACCESS
+        //    case "914": //IRCERR_DUPACCESS
+        //    case "915": //IRCERR_MISACCESS
+        //    case "916": //IRCERR_TOOMANYACCESSES
+        //        onAccessNRelatedReplies(toks[1], ircmsg);
+        //        break;
 
-            case "818":
-            case "819":
-                onPropReplies(toks[1], toks[3], ircmsg);
-                break;
+        //    case "900": //IRCERR_BADCOMMAND
+        //    case "901": //IRCERR_TOOMANYARGUMENTS
+        //    case "925": //IRCERR_TOOMANYARGUMENTS
+        //        onAccessNRelatedReplies(toks[1], ircmsg);
+        //        break;
 
-            default:
-                if (isNaN(toks[1]) == false) {
-                    if (toks[1] == "432" && _bConnectionRegistered) {
-                        if (this.UserName[0] != ">") break;
-                    }
-                    onErrorReplies(toks[1], toks[2], toks[3], strip(toks.slice(4).join(" ")));
-                }
+        //    case "818":
+        //    case "819":
+        //        onPropReplies(toks[1], toks[3], ircmsg);
+        //        break;
 
-                //unhandledCommand(ircmsg);
-                break;
-        }
-        // End of switch
+        //    default:
+        //        if (isNaN(toks[1]) == false) {
+        //            if (toks[1] == "432" && _bConnectionRegistered) {
+        //                if (this.UserName[0] != ">") break;
+        //            }
+        //            onErrorReplies(toks[1], toks[2], toks[3], strip(toks.slice(4).join(" ")));
+        //        }
+
+        //        //unhandledCommand(ircmsg);
+        //        break;
+        //}
+        //// End of switch
     }
     // end if
 }
-// End of the function
+
+function PingReply(s: string) { //-- Function converstion completed 25-Dec-2016 HY
+    IRCSend("PONG " + s);
+}
+
+function Write(s: string) { //-- Function converstion completed 25-Dec-2016 HY
+    fnWriteToPresenter(s);
+}
